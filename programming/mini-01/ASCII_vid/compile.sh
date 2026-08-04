0ea11049f4639300f7e3e66d2473fe18886265af
Shicheng_Zhang-0003 <shicheng_zhang@outlook.com>
2026-08-04 15:31:21 +0800

    04/08/26, 153115 CST: Repo Confusion???

--- programming/mini-01/ASCII_vid/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-g++ -std=c++17 convert_ascii_image.cpp -o convert_ascii_image $(pkg-config --cflags --libs opencv4)
-./convert_ascii_image
--- programming/mini-01/ASCII_vid/readme.txt
+++
@@ -1,2 +0,0 @@
-Modified clone from Servet Gulgonaru (Code Fiction on Youtube, Github Repo name tarantino08)
-
